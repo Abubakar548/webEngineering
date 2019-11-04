349f09198bb20d4cd95d9d6a24eeea694ae94944
Abubakar548 <bsef17m548@pucit.edu.pk>
2019-11-04 23:45:24 +0500

    Added Traverse (Forward,Backward,First,Last,Absolute) Functionality

--- Lab6/Driver.java
+++ Lab6/Driver.java
@@ -1,26 +1,40 @@
-import java.util.*;
-
 import javax.swing.JOptionPane;
 
-class Driver{
-    public static void main(String [] args)
-    {
-        signup su = new signup ();
-    
-    String inpt  = JOptionPane.showInputDialog(null, "Enter 1 to create a ACCOUNT\nEnter 2 to signin");
-    int option = Integer.parseInt(inpt);
-    switch(option)
-    {
-        case 1:
-        
-        su.createAccount();
-        break;
-        case 2:
-        su.sigin();
-        break;
+class Driver {
+    public static void main(String[] args) {
+        signup su = new signup();
+        int option = 1;
+        while (option != 0 && option < 8) {
+            String inpt = JOptionPane.showInputDialog(null,
+                    "Enter 1 to create a ACCOUNT\nEnter 2 to Sign in\nEnter 3 to Show Data Forward\nEnter 4 to Show Data Backward\nEnter 5 to Show First Data\nEnter 6 to Show Last Data\nEnter 7 to Select Row to Show\nAny Key for Exit");
+            option = Integer.parseInt(inpt);
 
-    }
+            switch (option) {
+            case 1:
+                su.createAccount();
+                break;
+            case 2:
+                su.sigin();
+                break;
+            case 3:
+                su.showdataForward();
+                break;
+            case 4:
+                su.showdataBackward();
+                break;
+            case 5:
+                su.showdataFirst();
+                break;
+            case 6:
+                su.showdataLast();
+                break;
+            case 7:
+                su.showdataAbsolute();
+                break;
 
+            }// Switch case
+        } // While Loop
 
-    }
-}
+    }// Main Function
+
+}// DRIVER CLASS
--- Lab6/signup.java
+++ Lab6/signup.java
@@ -3,216 +3,325 @@ import java.sql.*;
 
 import javax.swing.JOptionPane;
 
-class signup{
+class signup {
 
-    void createAccount()
-    {
-    try{
-           
-    Class.forName("com.mysql.jdbc.Driver");
-    String url = "jdbc:mysql://127.0.0.1/lab6";
-    Connection con=DriverManager.getConnection(url,"root","admin");
-    Statement st=con.createStatement();
+    void createAccount() {
+        try {
 
-	Scanner inp = new Scanner(System.in);
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://127.0.0.1/lab6";
+            Connection con = DriverManager.getConnection(url, "root", "admin");
+            Statement st = con.createStatement();
 
-	System.out.print("Please enter First Name: ");
-	String f_name = inp.nextLine();
-        
-	System.out.print("Please enter Last Name: ");
-	String l_name = inp.nextLine();
-        
-	System.out.print("Please enter password: ");
-	String pass = inp.nextLine();
+            Scanner inp = new Scanner(System.in);
 
-	System.out.print("Please enter Email: ");
-	String address = inp.nextLine();
+            System.out.print("Please enter First Name: ");
+            String f_name = inp.nextLine();
 
-	System.out.print("Please enter gender: ");
-	String gender= inp.nextLine();
-	
-	System.out.print("Please enter dob: ");
-	String dob = inp.nextLine();
+            System.out.print("Please enter Last Name: ");
+            String l_name = inp.nextLine();
 
+            System.out.print("Please enter password: ");
+            String pass = inp.nextLine();
 
-     String query="insert into info(f_name,l_name,pass,address,gender,dob) values('"+f_name+"','"+l_name+"', '"+pass+"', '"+address+"', '"+gender+"' , '"+dob+"' )";
+            System.out.print("Please enter Email: ");
+            String address = inp.nextLine();
 
-     int rs = st.executeUpdate( query );
+            System.out.print("Please enter gender: ");
+            String gender = inp.nextLine();
 
-	 System.out.println(rs);
-   
-     if(rs > 0){
-        System.out.println("Record inserted successfully.");
-	  }
-     
-     else{
-    	 System.out.println("Record could not inserted.");
-         }
+            System.out.print("Please enter dob: ");
+            String dob = inp.nextLine();
 
+            String query = "insert into info(f_name,l_name,pass,address,gender,dob) values('" + f_name + "','" + l_name
+                    + "', '" + pass + "', '" + address + "', '" + gender + "' , '" + dob + "' )";
 
-         con.close();
+            int rs = st.executeUpdate(query);
 
-        } //try
-        catch(SQLException ex)
-        {
+            System.out.println(rs);
+
+            if (rs > 0) {
+                System.out.println("Record inserted successfully.");
+            }
+
+            else {
+                System.out.println("Record could not inserted.");
+            }
+
+            inp.close();
+            con.close();
+
+        } // try
+        catch (SQLException ex) {
             System.out.println(ex);
-        }
-        catch(Exception ex)
-        {
+        } catch (Exception ex) {
             System.out.println(ex);
-            
+
         }
 
     }// createAccount
 
- 
+    void sigin() {
+        try {
 
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://127.0.0.1/lab6";
+            Connection con = DriverManager.getConnection(url, "root", "admin");
+            Statement st = con.createStatement();
 
+            Scanner inp = new Scanner(System.in);
 
+            System.out.print("Please enter Email: ");
+            String address = inp.nextLine();
 
-void sigin()
-{
-try{
-       
-Class.forName("com.mysql.jdbc.Driver");
-String url = "jdbc:mysql://127.0.0.1/lab6";
-Connection con=DriverManager.getConnection(url,"root","admin");
-Statement st=con.createStatement();
+            System.out.print("Please enter password: ");
+            String pass = inp.nextLine();
 
-Scanner inp = new Scanner(System.in);
+            String query = "select * from info where address = '" + address + "' and pass = '" + pass + "' ";
 
-System.out.print("Please enter Email: ");
-String address = inp.nextLine();
+            ResultSet rs = st.executeQuery(query);
 
-System.out.print("Please enter password: ");
-String pass = inp.nextLine();
+            if (rs.next()) {
+                String email = rs.getString("address");
+                String pwd = rs.getString("pass");
+                if (email.equals(address) && pass.equals(pwd)) {
+                    System.out.println("Sign In successfull");
 
+                    String inpt = JOptionPane.showInputDialog(null, "Enter 1 to update Info\nAny key else for exit");
+                    int option = Integer.parseInt(inpt);
+                    if (option == 1) {
+                        updateProfile(email);
+                    }
 
- String query="select * from info where address = '"+address+"' and pass = '"+pass+"' ";
+                }
 
- ResultSet rs = st.executeQuery( query );
-
- if(rs.next()){
-    String email = rs.getString("address");
-        String pwd = rs.getString("pass");
-        if(email.equals(address) && pass.equals(pwd))
-        {
-            System.out.println("Sign In successfull");
-            
-        String inpt  = JOptionPane.showInputDialog(null, "Enter 1 to update Info");
-        int option = Integer.parseInt(inpt);
-            if(option == 1)
-            {
-                updateProfile(email);
             }
+
+            else {
+                System.out.println("Sign IN UNSUCCESSFULL");
+            }
+
+            inp.close();
+
+        } // try
+        catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (Exception ex) {
+            System.out.println(ex);
+
         }
 
-  }
- 
- else{
-     System.out.println("Sign IN UNSUCCESSFULL");
-     }
+    }// signin
 
-    } //try
-    catch(SQLException ex)
-    {
-        System.out.println(ex);
-    }
-    catch(Exception ex)
-    {
-        System.out.println(ex);
-        
-    }
+    void updateProfile(String email) {
 
-}// signin
+        try {
 
-void updateProfile(String email)
-{
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://127.0.0.1/lab6";
+            Connection con = DriverManager.getConnection(url, "root", "admin");
+            Statement st = con.createStatement();
 
-    
+            Scanner inp = new Scanner(System.in);
 
+            System.out.print("Please enter First Name: ");
+            String f_name = inp.nextLine();
 
-    try{
-           
-        Class.forName("com.mysql.jdbc.Driver");
-        String url = "jdbc:mysql://127.0.0.1/lab6";
-        Connection con=DriverManager.getConnection(url,"root","admin");
-        Statement st=con.createStatement();
-    
-        Scanner inp = new Scanner(System.in);
-    
-        System.out.print("Please enter First Name: ");
-        String f_name = inp.nextLine();
-            
-        System.out.print("Please enter Last Name: ");
-        String l_name = inp.nextLine();
-            
-        System.out.print("Please enter password: ");
-        String pass = inp.nextLine();
-        
-        System.out.print("Please enter gender: ");
-        String gender= inp.nextLine();
-        
-        System.out.print("Please enter dob: ");
-        String dob = inp.nextLine();
-    
-    
-         String query="update info set f_name = '"+f_name+"', l_name = '"+l_name+"', pass = '"+pass+"', gender = '"+gender+"' , dob =  '"+dob+"'  where address= '" +email+ "' ";
-    
-         int rs = st.executeUpdate( query );
-    
-         System.out.println(rs);
-       
-         if(rs > 0){
-            System.out.println("Record updated successfully.");
-          }
-         
-         else{
-             System.out.println("Record could not updated.");
-             }
-    
-    
-             con.close();
-    
-            } //try
-            catch(SQLException ex)
-            {
-                System.out.println(ex);
+            System.out.print("Please enter Last Name: ");
+            String l_name = inp.nextLine();
+
+            System.out.print("Please enter password: ");
+            String pass = inp.nextLine();
+
+            System.out.print("Please enter gender: ");
+            String gender = inp.nextLine();
+
+            System.out.print("Please enter dob: ");
+            String dob = inp.nextLine();
+
+            String query = "update info set f_name = '" + f_name + "', l_name = '" + l_name + "', pass = '" + pass
+                    + "', gender = '" + gender + "' , dob =  '" + dob + "'  where address= '" + email + "' ";
+
+            int rs = st.executeUpdate(query);
+
+            System.out.println(rs);
+
+            if (rs > 0) {
+                System.out.println("Record updated successfully.");
             }
-            catch(Exception ex)
-            {
-                System.out.println(ex);
-                
+
+            else {
+                System.out.println("Record could not updated.");
             }
-    
 
+            inp.close();
+            con.close();
 
+        } // try
+        catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (Exception ex) {
+            System.out.println(ex);
 
+        }
 
+    }
 
+    public void showdataForward() {
 
+        try {
 
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://127.0.0.1/lab6";
+            Connection con = DriverManager.getConnection(url, "root", "admin");
+            Statement st = con.createStatement();
+            String query = "select * from info ";
 
+            ResultSet rs = st.executeQuery(query);
 
+            while (rs.next()) {
+                String email = rs.getString("address");
+                String pwd = rs.getString("pass");
+                JOptionPane.showMessageDialog(null, "Email:- " + email + "\nPassword:- " + pwd);
+            }
 
+            con.close();
 
+        } // try
+        catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (Exception ex) {
+            System.out.println(ex);
 
+        }
 
+    }// SHOW DATA FORWARD
 
+    public void showdataBackward() {
 
+        try {
 
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://127.0.0.1/lab6";
+            Connection con = DriverManager.getConnection(url, "root", "admin");
+            Statement st = con.createStatement();
+            String query = "select * from info ";
 
+            ResultSet rs = st.executeQuery(query);
+            if (rs.last())
+                do {
+                    String email = rs.getString("address");
+                    String pwd = rs.getString("pass");
+                    JOptionPane.showMessageDialog(null, "Email:- " + email + "\nPassword:- " + pwd);
+                } while (rs.previous());
+            JOptionPane.showMessageDialog(null, "No more Data to show!");
+
+            con.close();
+
+        } // try
+        catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (Exception ex) {
+            System.out.println(ex);
+
+        }
+
+    }// SHOW DATA BACKWARD
+
+    public void showdataFirst() {
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://127.0.0.1/lab6";
+            Connection con = DriverManager.getConnection(url, "root", "admin");
+            Statement st = con.createStatement();
+            String query = "select * from info ";
+
+            ResultSet rs = st.executeQuery(query);
+            if (rs.first()) {
+                String email = rs.getString("address");
+                String pwd = rs.getString("pass");
+                JOptionPane.showMessageDialog(null, "Email:- " + email + "\nPassword:- " + pwd);
+            } else
+                JOptionPane.showMessageDialog(null, "No Data to show!");
+
+            con.close();
+
+        } // try
+        catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (Exception ex) {
+            System.out.println(ex);
+
+        }
+
+    }// SHOW FIRST LINE
+
+    public void showdataLast() {
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://127.0.0.1/lab6";
+            Connection con = DriverManager.getConnection(url, "root", "admin");
+            Statement st = con.createStatement();
+            String query = "select * from info ";
+
+            ResultSet rs = st.executeQuery(query);
+            if (rs.last()) {
+                String email = rs.getString("address");
+                String pwd = rs.getString("pass");
+                JOptionPane.showMessageDialog(null, "Email:- " + email + "\nPassword:- " + pwd);
+            } else
+                JOptionPane.showMessageDialog(null, "No Data to show!");
+
+            con.close();
+
+        } // try
+        catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (Exception ex) {
+            System.out.println(ex);
+
+        }
+
+    }// SHOW LAST LINE
+
+    public void showdataAbsolute() {
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://127.0.0.1/lab6";
+            Connection con = DriverManager.getConnection(url, "root", "admin");
+            Statement st = con.createStatement();
+            String query = "select * from info ";
+
+            Scanner inp = new Scanner(System.in);
+
+            JOptionPane.showMessageDialog(null, "Please enter Row #: ");
+            Integer row = inp.nextInt();
+
+            ResultSet rs = st.executeQuery(query);
+            if (rs.absolute(row)) {
+                String email = rs.getString("address");
+                String pwd = rs.getString("pass");
+                JOptionPane.showMessageDialog(null, "Email:- " + email + "\nPassword:- " + pwd);
+            } else
+                JOptionPane.showMessageDialog(null, "No Data to show!");
+
+            inp.close();
+            con.close();
+
+        } // try
+        catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (Exception ex) {
+            System.out.println(ex);
+
+        }
+
+    }// SHOW ABSOLUTE LINE
 
 }
-
-
-}
-
-
-
-
-
-
-
-
-
